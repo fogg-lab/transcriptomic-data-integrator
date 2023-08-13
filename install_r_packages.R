c9dde49115d26c6bc7c6033eaaed443f43b76c28
Noah Wiggin <77313083+wigginno@users.noreply.github.com>
2023-08-13 17:31:23 -0400

    Add readr to install_r_packages.R

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,4 +1,4 @@
-install.packages(c('BiocManager', 'readr', repos='https://cloud.r-project.org/', Ncpus = 2)
+install.packages(c('BiocManager', 'readr'), repos='https://cloud.r-project.org/', Ncpus = 2)
 
 # Install preprocessCore without threading to avoid a multithreading bug using the rma function
 BiocManager::install('preprocessCore', configure.args='--disable-threading', force = TRUE)
